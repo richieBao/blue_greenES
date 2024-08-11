109c7e367ac371542958ef60d9caa691a0c84ef3
richie bao <46644000+richieBao@users.noreply.github.com>
2024-08-11 22:04:30 +0800

    accdb

--- utils/args.py
+++ utils/args.py
@@ -15,7 +15,7 @@ args=__C
 __C.db=AttrDict() 
 # __C.db.bng_db=f"database/bng_accdb.accdb" # r"C:\Users\richi\omen_richiebao\omen_github\blue_greenES\database\bng_accdb.accdb"
 # __C.db.bng_db=r"C:\Users\richi\omen_richiebao\omen_github\blue_greenES\database\bng_accdb.accdb"
-__C.db.bng_db= "database/bng_accdb.accdb" #"database/bng_accdb.accdb" 
+__C.db.bng_db= "/home/codingx/blue_greenES/database/bng_accdb.accdb" #"database/bng_accdb.accdb" 
 
 __C.raster=AttrDict() 
 __C.raster.glc_data_index={
